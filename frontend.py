660578d7cc404473b25b87954a3e546190b2802d
rajivvelury <110752281+rajivvelury@users.noreply.github.com>
2022-10-23 23:29:08 -0400

    Add files via upload

---
+++ frontend.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from PIL import Image
+
+st.title("Nightingale Mitosis Detection and Classificaiton")
+
+st.header("Nightingale")
+
+st.text("Please upload a WSI in .svs format to begin.")
